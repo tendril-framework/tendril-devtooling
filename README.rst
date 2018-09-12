790b26a1603e970260a42a7468e3544dedf57d34
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2018-09-12 08:47:59 +0530

    Initial extraction from tendril core

---
+++ README.rst
@@ -0,0 +1,15 @@
+========================================
+Generic Development Tooling from Tendril
+========================================
+
+.. image:: https://img.shields.io/travis/chintal/tendril-devtooling.svg
+        :target: https://travis-ci.org/chintal/tendril-utils-devtooling
+
+.. image:: https://img.shields.io/pypi/v/tendril-devtooling.svg
+        :target: https://pypi.python.org/pypi/tendril-devtooling
+
+* License: AGPLv3
+* Documentation: http://tendril-devtooling.readthedocs.org
+
+
+Generic Development Tooling from Tendril
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+    find_packages = None
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'pygments',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='tendril-devtooling',
+    version='0.1.1',
+    description="Generic Development Tooling from Tendril",
+    long_description=readme,
+    author="Chintalagiri Shashank",
+    author_email='shashank@chintal.in',
+    url='https://github.com/chintal/tendril-devtooling',
+    packages=find_packages(),
+    install_requires=requirements,
+    license="AGPLv3",
+    zip_safe=False,
+    keywords='tendril',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    # test_suite='tests',
+    # tests_require=test_requirements
+)
